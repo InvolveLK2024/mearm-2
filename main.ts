76ab827ee6c30b6ac0e16869356f89fb0fc67b5c
InvolveLK2024 <youthprojectletterkenny@involve.ie>
2024-09-11 18:34:41 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 control.raiseEvent(
-EventBusSource.MICROBIT_ID_BUTTON_A,
-EventBusValue.MICROBIT_EVT_ANY
+EventBusSource.MICROBIT_ID_BUTTON_B,
+EventBusValue.MICROBIT_BUTTON_EVT_UP
 )
 basic.forever(function () {
 	
